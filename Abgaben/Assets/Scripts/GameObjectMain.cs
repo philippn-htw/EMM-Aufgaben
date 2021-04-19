74e1e741da28a2cf807a6c77c8141c94c7f22341
Philipp Nitsche <s0573651@htw-berlin.de>
2021-04-19 18:46:28 +0200

    Coins Erstellt

--- Abgaben/Assets/Scripts/GameObjectMain.cs
+++ Abgaben/Assets/Scripts/GameObjectMain.cs
@@ -10,15 +10,17 @@ public class GameObjectMain : MonoBehaviour
     [SerializeField]
     private int objectCount = 10;
 
+
     // Start is called before the first frame update
     void Start()
     {
+
         for(int i=0;i<objectCount;i++) {
             Random.InitState(i);
             float x = Random.Range(-50.0f, 50.0f);
             float z = Random.Range(-50.0f, 50.0f);
             Vector3 pos = new Vector3(x,0,z);
-            Instantiate(myPrefab,pos,Quaternion.Euler(0,0,0));
+            Instantiate(myPrefab,pos,Quaternion.Euler(0,Random.Range(0.0f,360.0f),0));
         }
         
     }
@@ -26,6 +28,6 @@ public class GameObjectMain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
