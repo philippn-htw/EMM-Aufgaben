42eddbbd4aa82e0c767136ef705eaad0cebbcee4
Philipp Nitsche <s0573651@htw-berlin.de>
2021-04-20 16:44:42 +0200

    Enemies Added

--- Abgaben/Assets/Scripts/GameObjectMain.cs
+++ Abgaben/Assets/Scripts/GameObjectMain.cs
@@ -7,8 +7,13 @@ public class GameObjectMain : MonoBehaviour
 
     public Transform myPrefab;
 
+    public Transform enemy;
+
     [SerializeField]
     private int objectCount = 10;
+
+    [SerializeField]
+    private int enemyCount = 10;
 
 
     // Start is called before the first frame update
@@ -21,6 +26,14 @@ public class GameObjectMain : MonoBehaviour
             float z = Random.Range(-50.0f, 50.0f);
             Vector3 pos = new Vector3(x,0,z);
             Instantiate(myPrefab,pos,Quaternion.Euler(0,Random.Range(0.0f,360.0f),0));
+        }
+
+        for(int i=0;i<enemyCount;i++) {
+            Random.InitState(i+23);
+            float x = Random.Range(-50.0f, 50.0f);
+            float z = Random.Range(-50.0f, 50.0f);
+            Vector3 pos = new Vector3(x,0,z);
+            Instantiate(enemy,pos,Quaternion.Euler(0,Random.Range(0.0f,360.0f),0));
         }
         
     }
---
+++ Abgaben/Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehaviour : MonoBehaviour
+{
+
+    public Animator zombieAnim;
+
+    [SerializeField]
+    private float distance = 10.0f;
+    [SerializeField]
+    private float enemySpeed = 0.4f;
+
+    private GameObject player;
+
+    private bool isWalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        player = GameObject.Find("/Player");
+        zombieAnim = GetComponent<Animator>();
+        zombieAnim.Play("Stand");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerEnemyVector = player.transform.position - transform.position;
+        if(playerEnemyVector.magnitude < distance)
+        {
+            zombieAnim.Play("Walk");
+            Quaternion targetRotation = Quaternion.LookRotation(playerEnemyVector, Vector3.up);
+            transform.rotation = targetRotation;
+            transform.position += new Vector3(playerEnemyVector.x,0,playerEnemyVector.z) * Time.deltaTime * enemySpeed;
+            isWalking = true;
+        } else if(playerEnemyVector.magnitude > distance && isWalking) {
+            isWalking = false;
+            zombieAnim.Play("Stand");
+        }
+    }
+}
