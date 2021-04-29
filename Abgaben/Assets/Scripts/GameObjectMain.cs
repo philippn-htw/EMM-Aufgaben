ca1ccb863cda542bd5592fa9d8ee00db954fd738
Philipp Nitsche <s0573651@htw-berlin.de>
2021-04-29 18:45:51 +0200

    Basic AR Functions

--- Abgaben/Assets/Scripts/GameObjectMain.cs
+++ Abgaben/Assets/Scripts/GameObjectMain.cs
@@ -9,6 +9,8 @@ public class GameObjectMain : MonoBehaviour
 
     public Transform enemy;
 
+    public Transform arParent;
+
     [SerializeField]
     private int objectCount = 10;
 
@@ -25,7 +27,7 @@ public class GameObjectMain : MonoBehaviour
             float x = Random.Range(-50.0f, 50.0f);
             float z = Random.Range(-50.0f, 50.0f);
             Vector3 pos = new Vector3(x,0,z);
-            Instantiate(myPrefab,pos,Quaternion.Euler(0,Random.Range(0.0f,360.0f),0));
+            Instantiate(myPrefab,pos,Quaternion.Euler(0,Random.Range(0.0f,360.0f),0),arParent);
         }
 
         for(int i=0;i<enemyCount;i++) {
@@ -33,7 +35,7 @@ public class GameObjectMain : MonoBehaviour
             float x = Random.Range(-50.0f, 50.0f);
             float z = Random.Range(-50.0f, 50.0f);
             Vector3 pos = new Vector3(x,0,z);
-            Instantiate(enemy,pos,Quaternion.Euler(0,Random.Range(0.0f,360.0f),0));
+            Instantiate(enemy,pos,Quaternion.Euler(0,Random.Range(0.0f,360.0f),0),arParent);
         }
         
     }
